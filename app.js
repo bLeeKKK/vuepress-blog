cb2af8aa98a233363a161e576ddabbca6b3b5fce
lb <1961774471@qq.com>
2022-03-31 17:24:41 +0800

    1.准备提交

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 var http = require('http');
 var fs = require('fs');
-var server = http.createServer(function (request, response) {
-  fs.readFile(__dirname + "/dist/index.html", function (err, data) {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.end(data)
-  })
+var url = require('url');
+var server = http.createServer(function (req, res) {
+  var urls = url.parse(req.url, true)
+  if (urls.pathname === '/') {
+    fs.readFile(__dirname + "/dist/index.html", function (err, data) {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data)
+    })
+  } else {
+    fs.readFile(__dirname + "/dist" + req.url, function (err, data) {
+      if (err) {
+        res.end(data)
+      }
+    })
+  }
+  // fs.readFile(__dirname + "/dist/index.html", function (err, data) {
+  //   res.writeHead(200, { "Content-Type": "text/html" });
+  //   res.end(data)
+  // })
 })
 
 server.listen(3000)
